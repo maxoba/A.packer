7df6b68bbd5dedd9a951cfa80fd766d0644383d2
maxoba <132374228+maxoba@users.noreply.github.com>
2024-08-23 22:50:28 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,29 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "packerA_ami" {
+data "aws_ami" "packer" {
   owners      = ["self"] 
   most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["my-ubuntu-*"]
+  }
 }
 
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.packerA_ami.id
+resource "aws_instance" "packer" {
+  ami           = data.aws_ami.packer.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Packer-Terraform-Example"
+    Name = "Packer-Ami"
   }
+}
+
+output "ami-id" {
+  value = data.aws_ami.packer.id  
+}
+
+output "ami-name" {
+  value = data.aws_ami.packer.name 
 }
